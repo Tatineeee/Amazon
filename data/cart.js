eeb6576bc576e53e6ceea91d82a1d380395640c3
Tatineeee <tatineeee@gmail.com>
2025-01-17 14:23:07 +0800

    uploaded files

---
+++ data/cart.js
@@ -0,0 +1,39 @@
+export let cart = JSON.parse(localStorage.getItem('cart')) || {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+};
+
+export function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+    if (matchingItem) {
+        matchingItem.quantity++;
+    } else {
+        cart.push({
+            productId,
+            quantity: 1,
+        });
+    }
+    saveToStorage();
+    // console.log(cart);
+}
+
+export function removeItem(productId) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
+    saveToStorage();
+}
